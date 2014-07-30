18a514e781a99e4c2424d46bd250ed44619fd1f3
Ivan Milyutkin <ivan.milyutkin@gmail.com>
2014-07-31 00:27:08 +0300

    added Repositories for link and Url

---
+++ SearchEngine.Repositories/Impl/LinkRepository.cs
@@ -0,0 +1,18 @@
+﻿using MilyutkinI.Repository;
+using MilyutkinI.Repository.Contracts;
+using SearchEngine.DomainModels.Models;
+using SearchEngine.Repositories.Contracts;
+
+namespace SearchEngine.Repositories.Impl
+{
+    public class LinkRepository : BaseRepository<UrlLink>, ILinkRepository
+    {
+        public LinkRepository(ISourceContext sourceContext) : base(sourceContext)
+        {
+        }
+
+        public override void Dispose()
+        {
+        }
+    }
+}
---
+++ SearchEngine.Repositories/Impl/UrlRepository.cs
@@ -0,0 +1,18 @@
+﻿using MilyutkinI.Repository;
+using MilyutkinI.Repository.Contracts;
+using SearchEngine.DomainModels.Models;
+using SearchEngine.Repositories.Contracts;
+
+namespace SearchEngine.Repositories.Impl
+{
+    public class UrlRepository : BaseRepository<Url>, IUrlRepository
+    {
+        public UrlRepository(ISourceContext sourceContext) : base(sourceContext)
+        {
+        }
+
+        public override void Dispose()
+        {
+        }
+    }
+}
