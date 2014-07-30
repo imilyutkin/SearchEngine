c7a20782039855d69540c9f6bc744aa9610a915e
Ivan Milyutkin <ivan.milyutkin@gmail.com>
2014-07-30 23:57:33 +0300

    added models Url and UrlLink

---
+++ SearchEngine.DomainModels/Models/Url.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SearchEngine.DomainModels.Models
+{
+    public class Url : Entity
+    {
+        public String Title
+        {
+            get;
+            set;
+        }
+
+        [Index(IsUnique = true)]
+        public String Address
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ SearchEngine.DomainModels/Models/UrlLink.cs
@@ -0,0 +1,17 @@
+﻿namespace SearchEngine.DomainModels.Models
+{
+    public class UrlLink : Entity
+    {
+        public Url From
+        {
+            get;
+            set;
+        }
+
+        public Url To
+        {
+            get;
+            set;
+        }
+    }
+}
