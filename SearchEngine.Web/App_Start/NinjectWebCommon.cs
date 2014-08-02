abedbaa7542b2cfbf739f72a6981e4a50bddef3e
Ivan Milyutkin <ivan.milyutkin@gmail.com>
2014-08-02 18:09:06 +0300

    added ninject dependency

---
+++ SearchEngine.Web/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,74 @@
+using MilyutkinI.Repository.Contracts;
+using SearchEngine.DomainModels.Context;
+using SearchEngine.Repositories.Contracts;
+using SearchEngine.Repositories.Impl;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(SearchEngine.Web.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(SearchEngine.Web.App_Start.NinjectWebCommon), "Stop")]
+
+namespace SearchEngine.Web.App_Start
+{
+    using System;
+    using System.Web;
+
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+
+    public static class NinjectWebCommon 
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start() 
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+        
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+        
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+                RegisterServices(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind<ISourceContext>().To<SearchEngineSourceContext>();
+            kernel.Bind<IUrlRepository>().To<UrlRepository>();
+            kernel.Bind<ILinkRepository>().To<LinkRepository>();
+        }        
+    }
+}
---
+++ SearchEngine.Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SearchEngine.DomainModels.Models;
+using SearchEngine.Repositories.Contracts;
+
+namespace SearchEngine.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        protected IUrlRepository UrlRepository;
+
+        public HomeController(IUrlRepository urlRepository)
+        {
+            UrlRepository = urlRepository;
+        }
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            UrlRepository.Save(new Url
+            {
+                Title = "tut",
+                Address = "http://tut.by"
+            });
+            return View();
+        }
+
+    }
+}
